4bfbb75f738d5294fa7b06cf882fe69412bff105
Jonathan <jtlimads@gmail.com>
2026-03-23 18:45:27 -0300

    Insertion sort with reverse option

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -1,6 +1,41 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
 
-int main () {
+void insertionSort( std::vector<int>& v, bool reverse = false ) {
+    if (reverse) {
+        for ( int i = 1; i < v.size(); ++i ) {
+            auto carta = v[i];
+            auto j = i;
+            while ( j > 0 and v[j-1] < carta ) {
+                v[j] = v[j-1];
+                --j;
+            }
+            v[j] = carta;
+        }
+    } else {
+        for ( int i = 1; i < v.size(); ++i ) {
+            auto carta = v[i];
+            auto j = i;
+            while ( j > 0 and v[j-1] > carta ) {
+                v[j] = v[j-1];
+                --j;
+            }
+            v[j] = carta;
+        }
+    }
+    
+}
+
+int main() {
+
+    std::vector<int> v = {78, 19, 23, 45, 6, 2, 16};
+    insertionSort(v, true);
+    for ( auto x : v ) {
+        std::cout << x << ' ';
+    }
+
+    std::cout << std::endl;
+
     return 0;
+
 }
